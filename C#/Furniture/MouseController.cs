697f3d55fb7839b59021a801fffe2ee1abf7eb71
KG-03 <129266219+KG-03@users.noreply.github.com>
2026-04-28 22:00:10 +0900

    Create MouseController.cs
    Furniture 관련 클래스 중에 올리지 못한 클래스가 있어 추가.
    다음에 확인해야 하는 문서 미리 업로드.

---
+++ C#/Furniture/MouseController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    //mousePointerSprite    : 마우스 위치를 보여주기 위해 넣은 이미지.
+    //Floor                 : 바닥 오브젝트. mousePointerSprite의 위치를 제한하기 위해서 가져왔음.
+                            //만약 따로 오브젝트 설정을 하지 않는다면 new GameObject를 생성해야 함.
+
+    GameObject mousePointer;    //생성할 '마우스 포인터'
+
+    [Header("Plz Setting")]
+    public Sprite mousePointerSprite;
+
+    //현재 마우스 포인터가 가르키는 지점을 저장
+    float curMousePosX;
+    float curMousePosY;
+
+    Vector3 lastMousePos;
+
+    //UI에서 누른 가구의 시리얼 넘버를 저장. 이후 FurnitureSet에 FurnitureManager를 통해 전달함.
+    int furniture_sn;
+    string furniture_type;
+
+    void Start()
+    {
+        mousePointer = new GameObject("MousePointer");
+        mousePointer.AddComponent<MouseTrigger>();
+
+        mousePointer.AddComponent<SpriteRenderer>().sprite = mousePointerSprite;
+        mousePointer.AddComponent<BoxCollider2D>().isTrigger = true;
+        mousePointer.AddComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        mousePointer.GetComponent<Rigidbody2D>().simulated = false;
+
+        mousePointer.transform.localScale = mousePointer.transform.localScale * 0.89f;
+
+        FurnitureManager.F.SetOb_mouseController(mousePointer);
+    }
+
+    void Update()
+    {
+        //마우스 포인터가 보일지 말지 결정하는 것. FurnitureManager.cs의 furnitureState를 받아와서 결정.
+        if (FurnitureManager.F.GetFurnitureState() != 0)
+        {
+            mousePointer.gameObject.SetActive(true);
+            lastMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            lastMousePos.z = 0;
+
+            //칸 단위 지정이 너무 어려워져서 기능 포기.
+            mousePointer.transform.position = lastMousePos;
+        }
+        else if (FurnitureManager.F.GetFurnitureState() == 0)
+        {
+            mousePointer.gameObject.SetActive(false);
+        }
+    }
+
+    public void SetSerialNumber_MC(int i) { furniture_sn = i; }
+    public int GetSerialNumber_MC() {  return furniture_sn; }
+
+    public void SetType_MC(string s) { furniture_type = s; }
+    public string GetType_MC() { return furniture_type; }
+}
