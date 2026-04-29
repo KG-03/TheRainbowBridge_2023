bd1c41d9037fbdf89fb9ad3f0e67a6874a54ccab
KG-03 <129266219+KG-03@users.noreply.github.com>
2026-04-29 21:02:46 +0900

    Update MouseController.cs
    void Start()
    void Update()
    
    MouseController.cs 확인 완료.

--- C#/Furniture/MouseController.cs
+++ C#/Furniture/MouseController.cs
@@ -12,6 +12,7 @@ public class MouseController : MonoBehaviour
 
     GameObject mousePointer;    //생성할 '마우스 포인터'
 
+    //2026: 기본적으로 '인게임에서 사용하는 마우스 커서 이미지'를 사용하나, 가구 배치 때에는 해당 이미지를 '가구 이미지'로 교체하여 사용한다.
     [Header("Plz Setting")]
     public Sprite mousePointerSprite;
 
@@ -30,6 +31,7 @@ public class MouseController : MonoBehaviour
         mousePointer = new GameObject("MousePointer");
         mousePointer.AddComponent<MouseTrigger>();
 
+        //2026: 마우스 포인터를 생성하며 '마우스 포인터 오브젝트의 위치 확인'과 '충돌 검사'만 하기 위한 작업.
         mousePointer.AddComponent<SpriteRenderer>().sprite = mousePointerSprite;
         mousePointer.AddComponent<BoxCollider2D>().isTrigger = true;
         mousePointer.AddComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
@@ -43,6 +45,7 @@ public class MouseController : MonoBehaviour
     void Update()
     {
         //마우스 포인터가 보일지 말지 결정하는 것. FurnitureManager.cs의 furnitureState를 받아와서 결정.
+        //2026: FurnitureState가 0이면 하우징을 하지 않는 때, 0이 아니면 하우징을 하는 때.
         if (FurnitureManager.F.GetFurnitureState() != 0)
         {
             mousePointer.gameObject.SetActive(true);
